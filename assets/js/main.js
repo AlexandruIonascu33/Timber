bd11065c209271705c98602568e09695b8bd8ab8
AlexandruIonascu33 <79159257+AlexandruIonascu33@users.noreply.github.com>
2023-12-14 00:03:38 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
-/**
-* Template Name: Lumia
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/lumia-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+
+function() {
   "use strict";
 
   /**
@@ -258,4 +252,4 @@
    */
   new PureCounter();
 
-})()
+}()
